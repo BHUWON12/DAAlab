eee7a44f185d05cb9eff415e41c23762539846a9
Bhuwan Singh  Bhandari <116028881+BHUWON12@users.noreply.github.com>
2024-08-03 12:58:20 +0530

    Update 5bruteforce.py

--- 5bruteforce.py
+++ 5bruteforce.py
@@ -1,35 +1,22 @@
 def power_bruteforce(a, n):
-
- result = 1
-
- for i in range(n):
-
-  result = a
-
- return result
+    result = 1
+    for i in range(n):
+        result *= a
+    return result
 
 def power_divide_conquer(a, n):
+    if n == 0:
+        return 1
+    elif n % 2 == 0:
+        return power_divide_conquer(a * a, n // 2)
+    else:
+        return a * power_divide_conquer(a * a, n // 2)
 
-  if n == 0:
-
-   return 1
-
-  elif n % 2 == 0:
-
-   return power_divide_conquer(a* a, n // 2)
-
-  else:
-
-   return a power_divide_conquer(a*a, n // 2)
-
-#Main Code
-
+# Main Code
 a, n = map(int, input("Enter the value of a and n: ").split())
 
-result_brute power_bruteforce(a, n)
-
+result_brute = power_bruteforce(a, n)
 result_divide_conquer = power_divide_conquer(a, n)
 
 print("Result using brute force:", result_brute)
-
 print("Result using divide and conquer:", result_divide_conquer)
